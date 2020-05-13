6bf30c45d0a72ef5f0c348d8314e03ef827224ad
mfierd <muhammad.2789@students.amikom.ac.id>
2020-05-13 23:25:00 +0700

    polymorphism interface

---
+++ interfacePolymorphism/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PolymorphismInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Pilih Printer:");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+            Console.Write("Nomor Printer [1 ... 3] : ");
+            int pilihPrinter = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Printerw printer = new printer();
+
+            if (pilihPrinter > 0 && pilihPrinter < 4)
+            {
+                if (pilihPrinter == 1)
+                {
+                    printer = new Epson();
+                }
+                else if (pilihPrinter == 2)
+                {
+                    printer = new Canon();
+                }
+                else
+                {
+                    printer = new LaserJet();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Pilihan tidak valid \n");
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Nama\t: Muh. Firdaus F.");
+            Console.WriteLine("NIM\t: 19.11.2789");
+            Console.WriteLine("Kelas\t: IF03");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ interfacePolymorphism/epson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismInterface
+{
+    class Epson : Printerw
+    {
+        public void Show()
+        {
+            Console.WriteLine("Printer Epson dimension: 10 * 11");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Epson printer printing ...");
+        }
+    }
+}
